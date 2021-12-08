a0b54184c5e5b1c5dc7d25d90f8c5fad81b65ccf
Piyush219 <piyush66642k@gmail.com>
2021-12-08 15:47:03 +0530

    Passing Data using Loop

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
-function ExpenseItem(){
-    return <div>
-        <h2>Expense item!</h2>
-        <h5>Food Rs 10</h5>
-        <h5>Petrol Rs 10</h5>
-        <h5>Movie Rs 10</h5>
+import './ExpenseItem.css'
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className =" expense-item__description">
+        <h2>{props.title}</h2>
+        <h2>{props.location}</h2>
+        <div className = "expense-item__price">${props.amount}</div>
+      </div>
     </div>
+  );
 }
-
 
 export default ExpenseItem;
