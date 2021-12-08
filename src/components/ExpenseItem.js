903ab241933a039f245d062cb840871943f0b239
Piyush219 <piyush66642k@gmail.com>
2021-12-08 12:52:46 +0530

    With React

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return <div>
+        <h2>Expense item!</h2>
+        <h5>Food Rs 10</h5>
+        <h5>Petrol Rs 10</h5>
+        <h5>Movie Rs 10</h5>
+    </div>
+}
+
+
+export default ExpenseItem;
