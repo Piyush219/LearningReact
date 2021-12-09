91146411a2d4ea4276cdee0d3d4e2122241a148c
Piyush219 <piyush66642k@gmail.com>
2021-12-09 14:16:24 +0530

    working with useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,34 @@
-import './ExpenseItem.css'
-import Card from '../UI/Card.js';
-import ExpenseDate from './ExpenseDate';
+import React, { useState } from "react";
+// useState is a react hook
+import "./ExpenseItem.css";
+import Card from "../UI/Card.js";
+import ExpenseDate from "./ExpenseDate";
 // import ExpenseDetails from './ExpenseDetails';
 const ExpenseItem = (props) => {
-    const clickHandler = () => {
-      console.log('Clicked!!!')
-    }
-    const clickHandlerForDelete = () =>{
-      console.log('Expense Deleted !!')
-    }
+  const [title, setTitle] = useState(props.title); // These must be called directly inside the function, not outside not nested
+  const [amount, setAmount] = useState(props.amount);
+  
+  const clickHandler = () => {
+    setTitle('Updated')
+    //console.log('checking' + title)
+  };
+  const clickHandlerForChangeExpense = () => {
+    setAmount('100')
+    console.log("Expense Changed !!");
+  };
   return (
     <Card className="expense-item">
-      <ExpenseDate date= {props.date}/>
-      <div className =" expense-item__description">
-        <h2>{props.title}</h2>
+      <ExpenseDate date={props.date} />
+      <div className=" expense-item__description">
+        <h2>{title}</h2>
         {/* <h2>{props.location}</h2> */}
-        <div className = "expense-item__price">${props.amount}</div>
+        <div className="expense-item__price">${amount}</div>
       </div>
-      <button onClick ={clickHandler}>Change Title</button>
-      <button onClick = {clickHandlerForDelete}>Delete Expense</button>
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={clickHandlerForChangeExpense}>Change Expense</button>
       {/* <ExpenseDetails title = {props.title} amount = {props.amount} location = {props.location} /> */}
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
