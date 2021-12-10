c64e9b761b595f6b4b8a812b5116b62c3a48ff23
Piyush219 <piyush66642k@gmail.com>
2021-12-10 15:31:37 +0530

    Filtering on the basis of Year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,28 +5,30 @@ import React, { useState } from "react";
 import ExpensesFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(expense => {
+      return expense.date.getFullYear().toString() === filteredYear;
+  });
   return (
+    <div>
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.items.map((expense) => {
-        return (
+      {filteredExpenses.map((expense) => (
           <ExpenseItem
             key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
           />
-        );
-      })}
+      ))}
       {/* <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -48,6 +50,7 @@ const Expenses = (props) => {
         date={props.items[3].date}
       /> */}
     </Card>
+    </div>
   );
 };
 
